475e925a80d2760be6474d631ef3ea45056e3cec
AgentAashik <aashikibrahim2@gmail.com>
2025-07-23 12:36:06 +0530

    second commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 // src/context/AuthContext.js
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { isLoggedIn as checkLogin, logout as doLogout } from '../services/auth';
 
 const AuthContext = createContext();
